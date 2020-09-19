71a20a0b9eff944192eb83de6dcfe29632feea2d
uday <udayjagtap10@gmail.com>
2020-09-19 13:09:45 +0530

    Version 1.0.0

---
+++ Code.py
@@ -0,0 +1,92 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+import streamlit as st
+
+
+
+class plot:
+    def Count_Record(self,data):
+        st.header("Overview of dataset.")
+        st.write(data.head(10))
+        fig, ax = plt.subplots(figsize=(15,10))
+        region_count = sns.countplot(data=data, x="region")
+        region_count.set_xticklabels(region_count.get_xticklabels(), rotation=45)
+        st.pyplot(fig)
+
+    def General_Overview(self,data):
+        fig, ax = plt.subplots(5, 1, figsize=(30, 40), )
+        col = ['fertility', 'life', 'population', 'child_mortality', 'gdp']
+        for index, col1 in enumerate(col):
+            print(index, col1)
+            mean1 = data.groupby("Year")[col1].mean()
+            mean_plt = sns.barplot(x=mean1.index, y=mean1.values, ax=ax[int(index)])
+            mean_plt.set(xlabel="Year", ylabel=col1)
+            mean_plt.set_yticklabels(mean_plt.get_ylabel(),size = 10)
+            plt.setp(mean_plt.get_xticklabels(), rotation=45)
+            fig.tight_layout(pad=2.0)
+        st.pyplot(fig)
+
+
+    def Region_vs_All(self,data,Name=None):
+        if Name==None:
+            fig, ax = plt.subplots(5, 1, figsize=(25, 35))
+            col = ['fertility', 'life', 'population', 'child_mortality', 'gdp']
+            for index, col1 in enumerate(col):
+                print(index, col1)
+                mean1 = data.groupby("region")[col1].mean()
+                mean_plt = sns.barplot(x=mean1.index, y=mean1.values, ax=ax[int(index)])
+                mean_plt.set(ylabel=col1)
+
+                plt.setp(mean_plt.get_xticklabels(), rotation=10)
+                fig.tight_layout(pad=2.0)
+            st.pyplot(fig)
+        else:
+            sub_data = data.loc[data.region == Name]
+            fig, ax = plt.subplots(5, 1, figsize=(30, 40))
+            col = ['fertility', 'life', 'population', 'child_mortality', 'gdp']
+            for index, col1 in enumerate(col):
+                print(index, col1)
+                mean1 = sub_data.groupby("Year")[col1].mean()
+                mean_plt = sns.barplot(x=mean1.index, y=mean1.values, ax=ax[int(index)])
+                mean_plt.set(ylabel=col1)
+                plt.setp(mean_plt.get_xticklabels(), rotation=45)
+                fig.tight_layout(pad=2.0)
+            st.pyplot(fig)
+
+
+    def Region_vs_Box(self,data):
+        print("Provide some indication of the data's symmetry and skewness." +
+              "Unlike many other methods of data display.\nBoxplots show outliers")
+        col = ['fertility', 'life', 'population', 'child_mortality', 'gdp']
+        fig, ax = plt.subplots(5, 1, figsize=(15, 25))
+        for index, col1 in enumerate(col):
+            sns.boxplot(data=data, x="region", y=col1, orient="v", ax=ax[index])
+
+    def County_wise_Analysis(self,data, Name):
+        sub_data = data.loc[data.Country == Name]
+        if sub_data.shape[0] == 0:
+            return st.write("Please check the spelling of country..")
+        fig, ax = plt.subplots(3, 2, figsize=(30, 35))
+        sns.lineplot(data=sub_data, x="Year", y="fertility", ax=ax[0][0])
+        sns.lineplot(data=sub_data, x="Year", y="life", ax=ax[0][1])
+        sns.lineplot(data=sub_data, x="Year", y="population", ax=ax[1][0])
+        sns.lineplot(data=sub_data, x="Year", y="child_mortality", ax=ax[1][1])
+        sns.lineplot(data=sub_data, x="Year", y="gdp", ax=ax[2][0])
+        st.pyplot(fig)
+class main_class:
+    def main(self,data,input1=None,input2=None):
+        try:
+            obj=plot()
+            if input1=="Select":
+                return obj.Count_Record(data)
+            if input1=="General Overview":
+                return obj.General_Overview(data)
+            elif input1 == "region wise":
+                return obj.Region_vs_All(data,input2)
+            elif input1 == "Based On Country":
+                return obj.County_wise_Analysis(data,input2)
+        except Exception as e:
+            st.write("Exception",e)
+
---
+++ app.py
@@ -0,0 +1,39 @@
+import streamlit as st
+import Code
+import pandas as pd
+global data
+
+def Start(data):
+    Country = set(data.Country)
+    # year = set(data.Year)
+    region = set(data.region)
+    Main=Code.main_class()
+    st.title("Gapminder Analysis(Kaggle)")
+    st.sidebar.title("Analysis Based On")
+    Option=st.sidebar.selectbox("Select Option",("Select","General Overview","Based On Region","Based On Country"))
+    if Option=="Select":
+        Main.main(data,"Select")
+    elif Option=="General Overview":
+        st.header("General Overview(World)")
+        Main.main(data,"General Overview")
+    elif Option=="Based On Region":
+        st.header("Based On Region(Overview)")
+        Main.main(data,"region wise", None)
+        region = st.sidebar.selectbox("Select Region", tuple(region))
+        st.header("Analysis of "+region+ " region.")
+        Main.main(data,"region wise", region)
+    elif Option=="Based On Country":
+        st.header("Based On Country")
+        Country = st.sidebar.selectbox("Select Country", tuple(Country))
+        st.header("Analysis of " + Country )
+        Main.main(data,"Based On Country",Country)
+
+if __name__=="__main__":
+
+    @st.cache
+    def load_dataset():
+        data = pd.read_csv("clean_data.csv")
+        data.drop("Unnamed: 0", axis=1, inplace=True)
+        return data
+    data = load_dataset()
+    Start(data)
